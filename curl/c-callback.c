8894b2f6771d9ca0d1c7af61144af9cb53f4d06a
Connor Olding <cloningdonor@gmail.com>
2012-06-26 07:26:28 -0700

    updated for new Go typedefs

--- curl/c-callback.c
+++ curl/c-callback.c
@@ -5,8 +5,8 @@
 
 /* for OPT_HEADERFUNCTION */
 size_t header_function( char *ptr, size_t size, size_t nmemb, void *ctx) {
-    void *go_header_func = (void *)goGetCurlField((uintptr)ctx, "headerFunction");
-    GoInterface *userdata = (GoInterface *)goGetCurlField((uintptr)ctx, "headerData");
+    void *go_header_func = (void *)goGetCurlField((GoUintptr)ctx, "headerFunction");
+    GoInterface *userdata = (GoInterface *)goGetCurlField((GoUintptr)ctx, "headerData");
 
     if (userdata == NULL) {
 	return goCallWriteFunctionCallback(go_header_func, ptr, size*nmemb, goNilInterface());
@@ -21,8 +21,8 @@ void *return_header_function() {
 
 /* for OPT_WRITEFUNCTION */
 size_t write_function( char *ptr, size_t size, size_t nmemb, void *ctx) {
-    void *go_write_func = (void *)goGetCurlField((uintptr)ctx, "writeFunction");
-    GoInterface *userdata = (GoInterface *)goGetCurlField((uintptr)ctx, "writeData");
+    void *go_write_func = (void *)goGetCurlField((GoUintptr)ctx, "writeFunction");
+    GoInterface *userdata = (GoInterface *)goGetCurlField((GoUintptr)ctx, "writeData");
 
     if (userdata == NULL) {
 	return goCallWriteFunctionCallback(go_write_func, ptr, size*nmemb, goNilInterface());
@@ -36,8 +36,8 @@ void *return_write_function() {
 
 /* for OPT_READFUNCTION */
 size_t read_function( char *ptr, size_t size, size_t nmemb, void *ctx) {
-    void *go_read_func = (void *)goGetCurlField((uintptr)ctx, "readFunction");
-    GoInterface *userdata = (GoInterface *)goGetCurlField((uintptr)ctx, "readData");
+    void *go_read_func = (void *)goGetCurlField((GoUintptr)ctx, "readFunction");
+    GoInterface *userdata = (GoInterface *)goGetCurlField((GoUintptr)ctx, "readData");
 
     if (userdata == NULL) {
 	return goCallReadFunctionCallback(go_read_func, ptr, size*nmemb, goNilInterface());
@@ -52,8 +52,8 @@ void *return_read_function() {
 
 /* for OPT_PROGRESSFUNCTION */
 int progress_function(void *ctx, double dltotal, double dlnow, double ultotal, double ulnow) {
-    void *go_progress_func = (void *)goGetCurlField((uintptr)ctx, "progressFunction");
-    GoInterface *clientp = (GoInterface *)goGetCurlField((uintptr)ctx, "progressData");
+    void *go_progress_func = (void *)goGetCurlField((GoUintptr)ctx, "progressFunction");
+    GoInterface *clientp = (GoInterface *)goGetCurlField((GoUintptr)ctx, "progressData");
 
     if (clientp == NULL) {
 	return goCallProgressCallback(go_progress_func, goNilInterface(),
